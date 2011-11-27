dece7cfa3e87df1853cd0fd8e86a1b12a463f62e
thomasebell <tommy.e.bell@gmail.com>
2011-11-26 22:28:50 -0600

    Implemented the ability for recorded files to appear in listView.  Added OnItemClick listener to the listView.

--- Four Track/gen/cc/co/eurdev/eurecorder/R.java
+++ Four Track/gen/cc/co/eurdev/eurecorder/R.java
@@ -22,18 +22,20 @@ public final class R {
         public static final int stop=0x7f020005;
     }
     public static final class id {
-        public static final int LinearLayout02=0x7f060007;
-        public static final int LinearLayout03=0x7f060004;
-        public static final int LinearLayout04=0x7f060002;
-        public static final int label=0x7f060000;
-        public static final int listView1=0x7f060008;
-        public static final int spinner1=0x7f060005;
-        public static final int textFreeSpace=0x7f060001;
-        public static final int textView1=0x7f060003;
-        public static final int toggleRecord=0x7f060006;
+        public static final int LinearLayout02=0x7f060008;
+        public static final int LinearLayout03=0x7f060005;
+        public static final int LinearLayout04=0x7f060003;
+        public static final int label=0x7f060001;
+        public static final int listView1=0x7f060009;
+        public static final int spinner1=0x7f060006;
+        public static final int textFreeSpace=0x7f060002;
+        public static final int textListItem=0x7f060000;
+        public static final int textView1=0x7f060004;
+        public static final int toggleRecord=0x7f060007;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- Four Track/src/cc/co/eurdev/eurecorder/Eurecord.java
+++ Four Track/src/cc/co/eurdev/eurecorder/Eurecord.java
@@ -1,5 +1,7 @@
 package cc.co.eurdev.eurecorder;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -9,8 +11,10 @@ import android.os.StatFs;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +29,11 @@ public class Eurecord extends Activity {
 	LinearLayout.LayoutParams params;
 	TextView freeSpaceView;
 	AudioRecorder ar;
+	String[] files;
+	ToggleButton toggleRecord;
+    Spinner sampleRateSpinner;
+    TextView listItem;
+    ListView listView;
 	
 	
 	
@@ -33,21 +42,33 @@ public class Eurecord extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        trackPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
         sdIsMounted = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        trackPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+        
+        
         freeSpaceView = (TextView)findViewById(R.id.textFreeSpace);
         
-        final ToggleButton toggleRecord = (ToggleButton) findViewById(R.id.toggleRecord);
+        toggleRecord = (ToggleButton) findViewById(R.id.toggleRecord); 
+        sampleRateSpinner = (Spinner) findViewById(R.id.spinner1);
+        listItem = (TextView) findViewById(R.id.textListItem);
+        listView = (ListView) findViewById(R.id.listView1);
         
-        final Spinner sampleRateSpinner = (Spinner) findViewById(R.id.spinner1);
+        listView.setTextFilterEnabled(true);
+        
+        
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
         		this, R.array.sample_rate_array, android.R.layout.simple_spinner_dropdown_item);
         sampleRateSpinner.setAdapter(spinnerAdapter);
         sampleRateSpinner.setSelection(2);
-
-        prepareResources();
-        //Toast.makeText(Eurecord.this, "Free space: " + (int)freeSpace() + "MB", Toast.LENGTH_LONG).show();
         
+        listView.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
+        	public void onItemClick(AdapterView<?> parent, View view,
+        			int position, long id) {
+        		Toast.makeText(getApplicationContext(), ((TextView) view).getText(), 
+        				Toast.LENGTH_SHORT).show();
+        	}
+        });
+
         
         toggleRecord.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
@@ -59,13 +80,13 @@ public class Eurecord extends Activity {
             		int minute = calendar.get(Calendar.MINUTE);
             		int hour = calendar.get(Calendar.HOUR_OF_DAY);
             		int day = calendar.get(Calendar.DAY_OF_MONTH);
-            		int month = calendar.get(Calendar.MONTH);
+            		int month = calendar.get(Calendar.MONTH) + 1;
             		int year = calendar.get(Calendar.YEAR);
             		
             		String sampleRateString = sampleRateSpinner.getSelectedItem().toString().replace(",", "");
             		int sampleRate = Integer.parseInt(sampleRateString);
             		
-            		String track = "eurecord_" + year + "-" + month + "-" + day + 
+            		String track = "Eurecord_" + year + "-" + month + "-" + day + 
             						"-" + hour + "." + minute + "." + second;
 
             		
@@ -76,9 +97,7 @@ public class Eurecord extends Activity {
             		}
             			
             		//Toast.makeText(Eurecord.this, trackPath+track+ " "+ sampleRate, Toast.LENGTH_LONG).show();
-            		
-            		prepareResources();
-            		toggleRecord.setText("Stop");
+
             		ar = AudioRecorder.getInstance(sampleRate);
             		try {
             			ar.setOutputFile(trackPath + track);
@@ -92,7 +111,7 @@ public class Eurecord extends Activity {
         			ar.stop();
             		ar.release();
             		Toast.makeText(Eurecord.this, "stopped", Toast.LENGTH_LONG).show();
-            		toggleRecord.setText("Record");
+            		prepareResources();
         		}
         		
 	
@@ -101,10 +120,27 @@ public class Eurecord extends Activity {
         
     }
     
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	prepareResources();
+    }
+    
     public void prepareResources() {
     	
     	if (sdIsMounted) {
     		freeSpaceView.setText("There is " + (int)freeSpace() + "MB of space left.");
+    		
+    		FilenameFilter filter = new FilenameFilter() {
+    			public boolean accept(File dir, String name) {
+    				return name.startsWith("Eurecord_") && (name.endsWith(".wav") || name.endsWith(".3gp"));
+    			}
+    		};
+    		File dir = new File(trackPath);
+    		files = dir.list(filter);
+    		listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, files));
+    		
+    		
     	}
     }
     
